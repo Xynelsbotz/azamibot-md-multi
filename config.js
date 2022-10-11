09d743c34b0a2dfa2564ffe4105cd4c697bf97a9
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-10-11 21:58:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['6285282250251'],
+	['6285854337533'],
 	['79309965233'],
-	['6282337245566', 'Syahrul A.S', true]
+	['6282337245566', 'Xynels', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
